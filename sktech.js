0f967ee7738de1b183cb9517ea291ba280e8270d
harumi-k <142060716+harumi-k@users.noreply.github.com>
2023-10-25 22:39:36 -0300

    adicionando arquivos css e js

---
+++ sktech.js
@@ -0,0 +1,38 @@
+
+
+sketch.js
+Salvo: 19 minutos atrás
+1
+function setup() {
+2
+  createCanvas(600, 600);
+3
+  background("white");
+4
+}
+5
+​
+6
+function draw() {
+7
+  
+8
+  stroke("blue");
+9
+  fill("red");
+10
+​
+11
+  // console.log(mouseIsPressed);
+12
+​
+13
+  if (mouseIsPressed) {
+14
+    rect(mouseX, mouseY, 20, 35);
+15
+  }
+16
+}
+17
+​
